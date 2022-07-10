03321c5f74f5c062ffada96d5208596173e4ee1a
myakubek <michelleyakubek@gmail.com>
2022-07-09 18:29:29 -0700

    quick fix to mobile resize border lobby box

--- public/javascripts/mobile_versionify_lobby.js
+++ public/javascripts/mobile_versionify_lobby.js
@@ -11,11 +11,17 @@ function resizer() {
   if (window.innerWidth < 700){
     loginBox.style.textAlign = "center";
     loginBox.style.position = "inherit";
+    
+    loginBox.style.border = "none";
+
     leaderboard.style.width = window.innerWidth;
     leaderboard.style.overflowX = "auto";
     leaderboard.style.overflowY = "hidden";
   } else {
     loginBox.style.textAlign = "left";
     loginBox.style.position = "absolute";
+    
+    loginBox.style.borderLeft = "3px rgba(152, 152, 152, 0.8) solid";
+    loginBox.style.borderBottom = "2px rgba(195, 195, 195, 0.877) solid";
   }
 }
